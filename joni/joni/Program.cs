c8fca7ea68c771966fe44291f274c89850807782
joni <jonathanmcaceres@gmail.com>
2024-08-06 20:27:36 -0300

    primer commit con visual

---
+++ joni/joni/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace joni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Holis");
+
+            Console.ReadKey();
+        }
+    }
+}
